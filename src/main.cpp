404c669f3a8196b65417c9e6951d40ce96692683
stygmates <styg.tan.dat@gmail.com>
2019-02-23 13:44:04 +0100

    Add cli parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include "spdlog/spdlog.h"
 #include "ObjLoader.hpp"
+#include "CLI11.hpp"
 
 using namespace std;
-int main()
+int main(int argc, char **argv)
 {
-  spdlog::set_level(spdlog::level::debug);
+  CLI::App app{"Raytracing"};
+  std::string filename = "Yuna.obj";
+  app.add_option( "-f,--file", filename, "The obj to load" );
+  CLI11_PARSE( app, argc, argv );
+
+  spdlog::set_level( spdlog::level::debug );
   ObjLoader obj;
+  if( !obj.import( filename ) )
+  {
+    spdlog::error( "Failed to import file {}", filename );
+    return -1;
+  }
+
   return 0;
 }
