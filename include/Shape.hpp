a94832c148b9e8c10b7dbb082bbc8958878310be
stygmates <styg.tan.dat@gmail.com>
2019-03-14 16:22:45 +0100

    Now takes the ray into account

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -2,10 +2,11 @@
 #define __SHAPE_HPP
 #include <vector>
 #include <glm/glm.hpp>
+#include "Ray.hpp"
 
 class Shape{
 public:
-  virtual bool intersects_ray() = 0;
+  virtual bool intersects_ray(Ray r) = 0;
 private:
 };
 
