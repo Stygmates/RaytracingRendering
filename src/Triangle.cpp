2577ba9cff901a5b47d9658b30cd3cd3037ead6d
stygmates <styg.tan.dat@gmail.com>
2019-03-17 21:20:26 +0100

    Another sonarcloud test

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -18,5 +18,7 @@ bool Triangle::intersects_ray( Ray r )
 {
   glm::vec2 barycentre;
   float distance;
+  distance = 10;
+  distance = 5;
   return glm::intersectRayTriangle( r.get_origin(), r.get_direction(), vertices[ 0 ], vertices[ 1 ], vertices[ 2 ], barycentre, distance);
 }
